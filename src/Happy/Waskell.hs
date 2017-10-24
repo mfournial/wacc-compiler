c4517466a3758a215357d7fda729350fd703abe5
mfournial <mayeul.fournial@outlook.com>
2017-10-24 11:11:33 +0100

    Fix compilation for happy

--- src/Happy/Waskell.hs
+++ src/Happy/Waskell.hs
@@ -3,18 +3,12 @@ This module uses Happy, an automatic parser generator to produce the AST
 Module      : Happy.Waskell
 Description : Parser generator for WACC
 Maintainer  : mmf115@ic.ac.uk
+License     : ISC
 Stability   : experimental
 Portability : POSIX
 
 This module should not conflict with libraries and is designed to be imported
 qualified. E.g. @import qualified Happy.Waskell as H@
--}
-{-
-
-Copyright (C) 2017 Waskell
-
-READ LICENCE FILE
-
 -}
 
 module Happy.Wakell
@@ -25,5 +19,8 @@ module Happy.Wakell
 import qualified Data.Waskell as W
 import qualified Data.ByteString.Lazy as B
 
-generateAST :: [Token] -> WaccTree
+data Token = P Int
+           | NULL
+
+generateAST :: [Token] -> W.WaccTree
 generateAST = undefined

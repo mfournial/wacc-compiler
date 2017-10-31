64be955d6d399b5c557cc4c60d9f304099203266
Kyle-James Condon <kyle-james.condon16@ic.ac.uk>
2017-10-31 20:09:13 +0000

    Removed dependency on PrintWacc, as it is nearly impossible to fix given the timeframe

--- src/Bnfc/TestWacc.hs
+++ src/Bnfc/TestWacc.hs
@@ -9,7 +9,7 @@ import System.Exit ( exitFailure, exitSuccess )
 import LexWacc
 import ParWacc
 import SkelWacc
-import PrintWacc
+--import PrintWacc
 import AbsWacc
 
 
@@ -26,10 +26,10 @@ type Verbosity = Int
 putStrV :: Verbosity -> String -> IO ()
 putStrV v s = if v > 1 then putStrLn s else return ()
 
-runFile :: (Print a, Show a) => Verbosity -> ParseFun a -> FilePath -> IO ()
+runFile :: (Show a) => Verbosity -> ParseFun a -> FilePath -> IO ()
 runFile v p f = putStrLn f >> readFile f >>= run v p
 
-run :: (Print a, Show a) => Verbosity -> ParseFun a -> String -> IO ()
+run :: (Show a) => Verbosity -> ParseFun a -> String -> IO ()
 run v p s = let ts = myLLexer s in case p ts of
            Bad s    -> do putStrLn "\nParse              Failed...\n"
                           putStrV v "Tokens:"
@@ -37,17 +37,17 @@ run v p s = let ts = myLLexer s in case p ts of
                           putStrLn s
                           exitFailure
            Ok  tree -> do putStrLn "\nParse Successful!"
-                          showTree v tree
+                          --showTree v tree
 
                           exitSuccess
 
 
-showTree :: (Show a, Print a) => Int -> a -> IO ()
+{-showTree :: (Show a, Print a) => Int -> a -> IO ()
 showTree v tree
  = do
       putStrV v $ "\n[Abstract Syntax]\n\n" ++ show tree
       putStrV v $ "\n[Linearized tree]\n\n" ++ printTree tree
-
+-}
 usage :: IO ()
 usage = do
   putStrLn $ unlines

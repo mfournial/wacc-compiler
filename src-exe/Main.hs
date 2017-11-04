099d419b7fef187700f566a4c3277779860081ca
mfournial <mayeul.fournial@outlook.com>
2017-11-04 19:24:18 +0000

    Change Main to use new Error monad

--- src-exe/Main.hs
+++ src-exe/Main.hs
@@ -15,7 +15,6 @@ user input
 {-# OPTIONS_GHC -fno-warn-unused-imports -fno-warn-overlapping-patterns #-}
 
 import Control.Monad (when)
-import qualified Data.ByteString as B
 import System.Exit (ExitCode(ExitFailure, ExitSuccess), exitWith)
 import System.Environment (getArgs, getProgName)
 import System.IO (stdin, hGetContents)
@@ -26,42 +25,50 @@ import Bnfc.LexWacc
 import Bnfc.ParWacc
 import Bnfc.PrintWacc
 import Bnfc.SkelWacc
+import Data.Waskell.Error
 
-type ParseFun a = [Token] -> Err a
+-- | Definition for a parse function: takes a list of Tokens and produces an
+-- errorList possibly containing the ADT or the errors produced during parsing
+type ParseFunction a = [Token] -> ErrorList a
 
-myLLexer :: String -> [Token]
-myLLexer = myLexer
-
+-- | The Expected erbosity of the output
 type Verbosity = Int
 
-putStrV :: Verbosity -> String -> IO ()
+-- | putStrV == putStr if verbosity is superior to 1
+putStrV :: Verbosity -- ^ The desired verbosity
+        -> String -- ^ The string to print
+        -> IO ()
 putStrV v s = when (v > 1) $ putStrLn s
 
-runFile :: (Print a, Show a) => Verbosity -> ParseFun a -> FilePath -> IO ()
+-- | runFile will call run on the content of the passed in file
+runFile :: Verbosity -- ^ The desired level of verbosity
+        -> ParseFunction WaccTree -- ^ The desired parse function to use
+        -> FilePath -- ^ The file path 
+        -> IO ()
 runFile v p f = putStrLn f >> readFile f >>= run v p
 
-run :: (Print a, Show a) => Verbosity -> ParseFun a -> String -> IO ()
-run v p str = 
-    let ts =  myLLexer str
-    in case p ts of
-        Bad s   -> do  
-            putStrLn "\nParse              Failed...\n"
-            putStrV v "Tokens:"
-            putStrV v $ show ts
-            putStrLn s
-            exitWith $ ExitFailure 100
-        Ok  tree -> do
-            putStrLn "Parse Successfull!"
-            showTree v tree
-            exitWith ExitSuccess
+-- | run will run the parser on the inputed string and output the result if 
+-- the verbosity requires it. Run will always ouput errors
+run :: Verbosity -- ^ the desired verbosity
+    -> ParseFunction WaccTree -- ^ the parse function to use
+    -> String -- ^ the input to compiler
+    -> IO ()
+run v parser input = showTree v parseErr >> displayErrorsAndExit parseErr
+  where
+   parseErr :: ErrorList WaccTree
+   parseErr = myLexer input >>= parser
 
+-- | showTree will output the AST and a prettier version of the internal 
+-- representation of the program
+showTree :: Verbosity -- ^ desired verbosity
+         -> ErrorList WaccTree -- ^ the tree to parse
+         -> IO ()
+showTree v (ErrorList (Just tree) _) = do
+  putStrV v $ "\n[Abstract Syntax]\n\n" ++ show tree
+  putStrV v $ "\n[Linearized tree]\n\n" ++ printTree tree
+showTree _ (ErrorList Nothing _) = return ()
 
-showTree :: (Show a, Print a) => Int -> a -> IO ()
-showTree v tree
- = do
-      putStrV v $ "\n[Abstract Syntax]\n\n" ++ show tree
-      putStrV v $ "\n[Linearized tree]\n\n" ++ printTree tree
-
+-- | usage displays usage of the program CLI interface
 usage :: IO ()
 usage = do
   putStrLn $ unlines
@@ -69,7 +76,7 @@ usage = do
     , "  --help          Display this help message."
     , "  (no arguments)  Parse stdin verbosely."
     , "  (files)         Parse content of files verbosely."
-    , "  -s (files)      Silent mode. Parse content otherwise files silently."
+    , "  -v (files)      Verbose mode. Parse content otherwise files silently."
     ]
   exitWith $ ExitFailure 1
 
@@ -79,5 +86,5 @@ main = do
   case args of
     ["--help"] -> usage
     [] -> hGetContents stdin >>= run 2 pExp
-    "-s":fs -> mapM_ (runFile 0 pExp) fs
-    fs -> mapM_ (runFile 2 pExp) fs
+    "-v":fs -> mapM_ (runFile 2 pExp) fs
+    fs -> mapM_ (runFile 0 pExp) fs

5071920843be7f90f17910ca603b690861c7e606
Kyle-James Condon <kyle-james.condon16@ic.ac.uk>
2017-11-08 14:51:14 +0000

    Added doc to Util

--- src/Data/Waskell/Types/Util.hs
+++ src/Data/Waskell/Types/Util.hs
@@ -1,9 +1,23 @@
+{-|
+
+WaccType Type Utilities
+
+Group 26 -- Waskell
+Module      : ADT
+Maintainer  : kc1616@ic.ac.uk
+Portability : POSIX
+
+This module specifies utility functions for handling types.
+-}
+
 module Data.Waskell.Types.Util where
 
 import Data.Waskell.ADT
-  
+
+-- | liftType lifts a BaseType to be of Haskell Type Type (From our ADT)  
 liftType :: BaseType -> Type
 liftType = Pairable . BaseType
 
+-- | wplus lifts two BaseTypes and then pairs them
 wplus :: BaseType -> BaseType -> (Type, Type)
 wplus a b = (liftType a, liftType b)

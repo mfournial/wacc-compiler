8e2e80880f4a40d779236ab34e4355b392339119
Pranav Kalidindi <pvk16@ic.ac.uk>
2017-11-11 16:01:17 +0000

    Built Instrunction set | Re-order set, errors

--- src/Code/Instructions.hs
+++ src/Code/Instructions.hs
@@ -3,38 +3,71 @@ module Code.Instructions where
 --genUniqueLabel a = unsafePerfomeIO(return $ a ++ getTime)
 
 data Instr = Define String
-		   | Jsr String
-		   | Ret
-		   | Mov Reg Op
-		   | Add Reg Op Op
-		   | XOr Op Op Op
-		   | Or Reg Reg Op
-		   | And Op Op Op
-		   | Adds Op Op Op
-		   | Sub Op Op Op
-		   | Cmp Op Op
-		   | Mul Reg Reg Reg Reg
-		   | Div Op Op Op
-		   | Bra String
-		   | LSB Reg Op
-		   | Meq Reg Op
-		   | Mgt Reg Op
-		   | Mlt Reg Op
-		   | Mge Reg Op
-		   | Mle Reg Op
-		   | Mne Reg Op
-		   | Neg Reg Reg Op
+           | Ret
+		       | MOV Condition Set Reg Op2
+					 | MVN Condition Set Reg Op2
+					 | CMP Condition Reg Op2
+					 | CMN Condition Reg Op2
+					 | TEQ Condition Reg Op2
+					 | TST Condition Reg Op2
+					 | AND Condition Set Reg Reg Op2
+					 | EOR Condition Set Reg Reg Op2
+					 | BIC Condition Set Reg Reg Op2
+					 | ORR Condition Set Reg Reg Op2
+					 | SUB Condition Set Reg Reg Op2
+					 | RSB Condition Set Reg Reg Op2
+					 | ADD Condition Set Reg Reg Op2
+					 | MUL Condition Set Reg Reg Reg
+					 | MLA Condition Set Reg Reg Reg
+					 | LDR Condition Mem Reg Address
+					 | STR Condition Mem Reg Address
+					 | LDM Condition AddressMode Reg Regs
+					 | STM Condition AddressMode Reg Regs
+					 | PUSH Condition Regs
+					 | POP Condition Regs
+					 | Label String
 
-data Op = RegOp Reg
-		| ImmOp Reg Bool
-		| OffOp Reg Int
-		| IntOp Int
+
+
+data Op2 = ShiftReg Reg Shift  -- ^  Shift Reg
+         | ImmOp String        -- ^  Expression
+
+data Shift = ASL Int -- ^ Arithmetic Shift Left
+           | LSL Int -- ^ Logical Shift Left
+					 | LSR Int -- ^ Logical Shift Right
+					 | ASR Int -- ^ Arithmetic Shift Right
+					 | ROR Int -- ^ Rotate Right
+					 | RRX     -- ^ Rotate Right Extended
+
+data Mem = B   -- ^ Transfer Byte
+         | SB  -- ^ Sign-Extended byte (LDR Only)
+				 | H   -- ^ Transfer Halfword
+				 | SH  -- ^ Sign-Extended Halfword (LDR Only)
+				 | W   -- ^ Word
+
+data Address = Expression String  -- ^ expression (need to change!!)
+	           | OffReg Reg Offset  -- ^ offset (pre-index/ post-index)
+
+data AddressMode = IA  -- ^ Increment After
+                 | IB  -- ^ Increment Before
+								 | DA  -- ^ Decrement After
+								 | DB  -- ^ Decrement Before
+
+data Offset = Int Int            -- ^
+            | RegShift Reg Shift -- ^
 
 data Condition = Eq   -- ^ Equal
 			   | Neq  -- ^ Not equal
-			   | CS Bool   -- ^ Carry set
-			   | Ov Bool   -- ^ Overflow
-			   | GEq -- ^ Greater than or Equal
-			   | LEq -- ^ Less than or equal
-			   | Lt  -- ^ Less than 
-			   | Gt  -- ^ Greater than
+			   | CS   -- ^ Carry set
+			   | CC   -- ^ Carry clear
+				 | MI   -- ^ negative
+				 | PL   -- ^ zero or positive
+				 | VS   -- ^ overflow
+				 | VC   -- ^ no overflow
+				 | HI   -- ^ unsigned higher
+				 | LS   -- ^ unsigned lower or same
+				 | GE   -- ^ greater than or equal
+				 | LT   -- ^ less than
+				 | GT   -- ^ greater than
+				 | LE   -- ^ less than or equal
+				 | AL   -- ^ always

4f497aafdd370ff35c2046b9d4b1f5e8b1b50992
Kyle-James Condon <kyle-james.condon16@ic.ac.uk>
2017-11-14 16:23:38 +0000

    Removed positionable typeclass, created getVal function. defined genFuncCode.

--- src/Code/Generator.hs
+++ src/Code/Generator.hs
@@ -10,7 +10,13 @@ produceASM :: FilePath -> WaccTree -> IO ()
 produceASM f = (writeCode f) . genCode
 
 genCode :: WaccTree -> [Instr]
-genCode = undefined
+genCode (WaccTree (Program fs sb)) = concatMap (genFuncCode . getVal) fs ++ genCode' sb
+
+genCode' :: ScopeBlock -> [Instr]
+genCode' = undefined
+
+genFuncCode :: Function -> [Instr]
+genFuncCode (Function _ id _ sb)  = Define (getVal id) : genCode' sb
 
 writeCode :: FilePath -> [Instr] -> IO ()
 writeCode f = (writeFile f) . unlines . (map show)

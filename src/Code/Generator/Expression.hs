f9ef67481cf692e6fe1c4fa0012bb8d68d0dc025
Kyle-James Condon <kyle-james.condon16@ic.ac.uk>
2017-11-18 16:36:31 +0000

    Implemented basic expressions

--- src/Code/Generator/Expression.hs
+++ src/Code/Generator/Expression.hs
@@ -1,14 +1,23 @@
 module Code.Generator.Expression (expression, expressionReg) where
 
-import Code.Instructions(Reg)
+import Code.Instructions
 import Code.Generator.State
 import Code.Generator.RetLoc
 import Data.Waskell.ADT
 
 import Data.Sequence((><))
+import Data.Char(ord)
 
 expression :: Expression -> ARM (Instructions, RetLoc)
-expression e = undefined
+expression (IntExp i)       = intToReg i       R0
+expression (BoolExp True)   = intToReg 1       R0
+expression (BoolExp False)  = intToReg 0       R0
+expression (CharExpr c)     = intToReg (ord c) R0 
+
+expression _ = error "Expression pattern not matched"
+
+intToReg :: Int -> Reg -> ARM (Instructions, RetLoc)
+intToReg i r = return (pure (LDR AL W r (Const i)), Register r)
 
 expressionReg :: Expression -> Reg -> ARM Instructions
 expressionReg e r = do

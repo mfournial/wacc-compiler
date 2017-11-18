d6f614cd098c5ad2ab400b6528503af05dd27d35
Kyle-James Condon <kyle-james.condon16@ic.ac.uk>
2017-11-18 16:26:26 +0000

    Added register as a return option

--- src/Code/Generator/RetLoc.hs
+++ src/Code/Generator/RetLoc.hs
@@ -4,13 +4,16 @@ import Code.Instructions
 
 import Data.Sequence
 
-data RetLoc = HeapAddr Int | StackOffset Int | StringLit String | RegLoc Reg
+data RetLoc = HeapAddr Int | StackOffset Int | StringLit String | RegLoc Reg | Register Reg
 
 storeToRegister :: Reg -> RetLoc -> Instructions
 storeToRegister r (HeapAddr i)    = storeToRegister' r (Const i) >< storeToRegister r (RegLoc r)
 storeToRegister r (StackOffset i) = storeToRegister' r (OffReg StackPointer (addrToOffset i) False)   
 storeToRegister r (StringLit str) = storeToRegister' r (Label str)
 storeToRegister r' (RegLoc r) = storeToRegister' r' (OffReg r (addrToOffset 0) False)
+storeToRegister r' (Register r)
+  | r' == r = empty
+  | otherwise = pure (MOV AL F r' (ShiftReg r NSH))
 
 storeToRegister' :: Reg -> Address -> Instructions
 storeToRegister' r a = pure (LDR AL W r a)

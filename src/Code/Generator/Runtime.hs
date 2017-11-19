5e08064aa948680947e431071e3b9f9d3025e7db
mfournial <mayeul.fournial@outlook.com>
2017-11-19 18:01:43 +0000

    Implement printIntRuntime with runtimegenerator

--- src/Code/Generator/Runtime.hs
+++ src/Code/Generator/Runtime.hs
@@ -15,6 +15,24 @@ type RuntimeGenerator = ARM (RuntimeComponent, String)
 generateExitRuntime :: RuntimeGenerator 
 generateExitRuntime = undefined
 
+-- | Print char isn't a function - just put the char in R0 and call it
+generatePrintChrRuntime :: Instr -- ^ 
+generatePrintChrRuntime = BL AL "putchar"
+
+generatePrintIntRuntime :: RuntimeGenerator
+generatePrintIntRuntime = do
+  intloc <- newStringLiteral "%d\0"
+  let fname = "runtime_print_int" in
+    return (RC PrintStr ( Define fname
+                       <| PUSH [LinkRegister]
+                       <| storeToRegisterPure R1 (RegLoc R0) 
+                       >< (ADD AL F R0 R0 (ImmOpInt 4)
+                       <| BL AL "printf"
+                       <| MOV AL F R0 (ImmOpInt 0)
+                       <| BL AL "fflush"
+                       <| POP [PC]
+                       <| empty)), fname)
+
 generatePrintStrRuntime :: RuntimeGenerator
 generatePrintStrRuntime = do
  sloc <- newStringLiteral "%.*s\\0" 
@@ -31,13 +49,13 @@ generatePrintStrRuntime = do
 
 generateReadCharRuntime :: RuntimeGenerator
 generateReadCharRuntime = do
-  chloc <- newStringLiteral "%c\0"
+  chloc <- newStringLiteral " %c\0"
   let fname = "runtime_read_char" in
     return (RC PrintStr (Define fname <| scanfCall chloc), fname)
 
 generateReadIntRuntime :: RuntimeGenerator
 generateReadIntRuntime = do
-  intloc <- newStringLiteral " %d\0"
+  intloc <- newStringLiteral "%d\0"
   let fname = "runtime_read_int" in
     return (RC PrintStr (Define fname <| scanfCall intloc), fname)
 

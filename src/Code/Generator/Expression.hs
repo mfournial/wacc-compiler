38db697c9a5985c1782aedf91c0d04430f66ad34
Kyle-James Condon <kyle-james.condon16@ic.ac.uk>
2017-11-19 14:59:28 +0000

    First attempt at implementation of unary expressions

--- src/Code/Generator/Expression.hs
+++ src/Code/Generator/Expression.hs
@@ -4,11 +4,12 @@ module Code.Generator.Expression (expression, expressionReg) where
 
 import Code.Instructions
 import Code.Generator.State
+import Code.Generator.StateInstructions
 import Code.Generator.RetLoc
 import Code.Generator.RetLoc.Internal
 import Data.Waskell.ADT
 
-import Data.Sequence((><), empty)
+import Data.Sequence((><), (<|), (|>), empty, singleton)
 import Data.Char(ord)
 
 expression :: Expression -> ARM (Instructions, RetLoc)
@@ -20,11 +21,20 @@ expression PairExpr           = intToReg 0       R0
 
 expression (IdentExpr (s, _)) = fmap (empty,) $ getVar s
 
---expression (UExpr (UBang, _) (e, _)) = do
---  (ins, (Register r)) <- expression e
+expression (UExpr (uexp, _) (e, _)) = do
+  (sub, loc)          <- expression e
+  (savIns, [saveLoc]) <- push [R0]
+  strRegIns           <- storeToRegister R0 loc
+  (retIns, [retLoc])  <- push [R0]
+  restoreReg          <- storeToRegister R0 saveLoc
+  return $ ((sub >< (savIns <| (strRegIns >< ((evalUExp uexp |> retIns) >< restoreReg)))), retLoc)
 
 expression (StringExpr str) = fmap ((empty,) . PRL) $ newStringLiteral str
 expression _ = error "Expression pattern not matched"
+
+evalUExp :: UnaryOperator -> Instructions
+evalUExp UMinus = singleton (RSB AL F R0 R0 (ImmOpInt 0))
+evalUExp _ = error "UExp pattern not matched"
 
 intToReg :: Int -> Reg -> ARM (Instructions, RetLoc)
 intToReg i r = return (pure (LDR AL W r (Const i)), PRL (Register r))

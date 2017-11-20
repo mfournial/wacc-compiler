94b6747ca759a11cc9d0c2b7e9baa108ea218349
mfournial <mayeul.fournial@outlook.com>
2017-11-20 18:36:18 +0000

    Refactor functions names for runtime

--- src/Code/Generator/Runtime/Internal.hs
+++ src/Code/Generator/Runtime/Internal.hs
@@ -1,6 +1,7 @@
 module Code.Generator.Runtime.Internal where
 
 import Code.Instructions
+import Data.Maybe(fromJust)
 
 data RuntimeComponent = RC RCID Instructions
 
@@ -19,3 +20,20 @@ data RCID = PrintStr
           | ArrayCheck
           | Checkdbz
           deriving (Eq)
+
+names :: [(RCID, String)]
+names = [ (PrintStr, "runtime_print_string")
+        , (PrintInt, "runtime_print_int")
+        , (PrintChar, "runtime_print_char")
+        , (PrintBool, "runtime_print_bool")
+        , (PrintRef, "runtime_print_ref")
+        , (ReadInt, "runtime_read_int")
+        , (ReadChar, "runtime_read_char")
+        , (ThrowRuntimeErr, "runtime_throw_err")
+        , (FreePair, "runtime_free_pair")
+        , (ArrayCheck, "runtime_array_check")
+        , (Checkdbz, "runtime_check_division_by_zero")
+        ]
+
+label :: RCID -> String
+label = fromJust . flip lookup names

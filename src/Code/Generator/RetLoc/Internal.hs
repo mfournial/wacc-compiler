b0a8b5a6784bcc1050584e259779cb212cf5dbf9
mfournial <mayeul.fournial@outlook.com>
2017-11-20 11:55:13 +0000

    Add RegLoc offset to data type for regloc

--- src/Code/Generator/RetLoc/Internal.hs
+++ src/Code/Generator/RetLoc/Internal.hs
@@ -6,6 +6,7 @@ data RetLoc = PRL PureRetLoc | StackPtr Int
 data PureRetLoc = HeapAddr Int
                 | StringLit String
                 | RegLoc Reg
+                | RegLocOffset Reg Int
                 | Register Reg
                 | ImmChar Char
                 | ImmInt Int

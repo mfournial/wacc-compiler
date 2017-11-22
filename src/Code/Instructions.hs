abe790421fc3d14bb00b19dbf5f87e091d45912f
nathaniel <oluwaseun.oshunniyi16@imperial.ac.uk>
2017-11-22 11:51:05 +0000

    update codestyle ARM/Instructions

--- src/Code/Instructions.hs
+++ src/Code/Instructions.hs
@@ -7,105 +7,112 @@ class PrintARM a where
   printARM :: a -> String
 
 type Instructions = Seq Instr
-data Instr = Section String
-           | FunSection String
-           | Word Int
-           | Ascii String
-           | Define String
-           | Global
-           | MOV Condition Set Reg Op2
-           | MVN Condition Set Reg Op2
-           | B   Condition String
-           | BL  Condition String
-           | CMP Condition Reg Op2
-           | CMN Condition Reg Op2
-           | TEQ Condition Reg Op2
-           | TST Condition Reg Op2
-           | AND Condition Set Reg Reg Op2
-           | EOR Condition Set Reg Reg Op2
-           | BIC Condition Set Reg Reg Op2
-           | ORR Condition Set Reg Reg Op2
-           | SUB Condition Set Reg Reg Op2
-           | RSB Condition Set Reg Reg Op2
-           | ADD Condition Set Reg Reg Op2
-           | MUL Condition Set Reg Reg Reg
-           | MLA Condition Set Reg Reg Reg
-           | LDR Condition Mem Reg Address
-           | STR Condition Mem Reg Address
-           | PUSH [Reg]
-           | POP [Reg]
-           | DIVIDER
-           deriving (Eq)
+data Instr = 
+      Section String
+    | FunSection String
+    | Word Int
+    | Ascii String
+    | Define String
+    | Global
+    | MOV Condition Set Reg Op2
+    | MVN Condition Set Reg Op2
+    | B   Condition String
+    | BL  Condition String
+    | CMP Condition Reg Op2
+    | CMN Condition Reg Op2
+    | TEQ Condition Reg Op2
+    | TST Condition Reg Op2
+    | AND Condition Set Reg Reg Op2
+    | EOR Condition Set Reg Reg Op2
+    | BIC Condition Set Reg Reg Op2
+    | ORR Condition Set Reg Reg Op2
+    | SUB Condition Set Reg Reg Op2
+    | RSB Condition Set Reg Reg Op2
+    | ADD Condition Set Reg Reg Op2
+    | MUL Condition Set Reg Reg Reg
+    | MLA Condition Set Reg Reg Reg
+    | LDR Condition Mem Reg Address
+    | STR Condition Mem Reg Address
+    | PUSH [Reg]
+    | POP [Reg]
+    | DIVIDER
+  deriving (Eq)
 
-data Set = T
-         | F
-         deriving (Eq)
+data Set = T | F
+  deriving (Eq)
 
-data Reg = R0
-         | R1
-         | R2
-         | R3
-         | R4
-         | R5
-         | R6
-         | R7
-         | R8
-         | R9
-         | R10
-         | R11
-         | R12
-         | StackPointer
-         | LinkRegister
-         | PC
-         deriving (Eq, Enum, Show)
+data Reg = 
+      R0
+    | R1
+    | R2
+    | R3
+    | R4
+    | R5
+    | R6
+    | R7
+    | R8
+    | R9
+    | R10
+    | R11
+    | R12
+    | StackPointer
+    | LinkRegister
+    | PC
+  deriving (Eq, Enum, Show)
 
-data Op2 = ShiftReg Reg Shift  -- ^  Shift Register
-         | ImmOpInt Int        -- ^  #expression
-         | ImmOpCh Char        -- ^  #expression
-         deriving (Eq)
+data Op2 = 
+      ShiftReg Reg Shift -- ^  Shift Register
+    | ImmOpInt Int       -- ^  #expression
+    | ImmOpCh Char       -- ^  #expression
+  deriving (Eq)
 
-data Shift = ASL Int -- ^ Arithmetic Shift Left
-           | LSL Int -- ^ Logical Shift Left
-           | LSR Int -- ^ Logical Shift Right
-           | ASR Int -- ^ Arithmetic Shift Right
-           | ROR Int -- ^ Rotate Right
-           | RRX     -- ^ Rotate Right Extended
-           | NSH     -- ^ No Shift
-           deriving (Eq)
+data Shift = 
+      ASL Int -- ^ Arithmetic Shift Left
+    | LSL Int -- ^ Logical Shift Left
+    | LSR Int -- ^ Logical Shift Right
+    | ASR Int -- ^ Arithmetic Shift Right
+    | ROR Int -- ^ Rotate Right
+    | RRX     -- ^ Rotate Right Extended
+    | NSH     -- ^ No Shift
+  deriving (Eq)
 
-data Mem = UB  -- ^ Transfer Byte
-         | SB  -- ^ Sign-Extended byte (LDR Only)
-         | H   -- ^ Transfer Halfword
-         | SH  -- ^ Sign-Extended Halfword (LDR Only)
-         | W   -- ^ Word
-         deriving (Eq)
+data Mem = 
+      UB  -- ^ Transfer Byte
+    | SB  -- ^ Sign-Extended byte (LDR Only)
+    | H   -- ^ Transfer Halfword
+    | SH  -- ^ Sign-Extended Halfword (LDR Only)
+    | W   -- ^ Word
+  deriving (Eq)
 
-data Address = Label String                -- ^ expression
-             | Const Int                   -- ^ expression
-             | OffReg Reg Offset Bool      -- ^ offset (pre-index)
-             deriving (Eq)
+data Address = 
+      Label String           -- ^ expression
+    | Const Int              -- ^ expression
+    | OffReg Reg Offset Bool -- ^ offset (pre-index)
+  deriving (Eq)
 
 
-data Offset = Int Int             -- ^ value of the offset
-            | RegShift Reg Shift  -- ^ @ offset = [Reg] << shift
-            deriving (Eq)
+data Offset = 
+      Int Int             -- ^ value of the offset
+    | RegShift Reg Shift  -- ^ @ offset = [Reg] << shift
+  deriving (Eq)
 
-data Condition = Eq   -- ^ Equal
-               | Neq  -- ^ Not equal
-               | CS   -- ^ Carry set
-               | CC   -- ^ Carry clear
-               | MI   -- ^ negative
-               | PL   -- ^ zero or positive
-               | VS   -- ^ overflow
-               | VC   -- ^ no overflow
-               | HI   -- ^ unsigned higher
-               | LS   -- ^ unsigned lower or same
-               | GE   -- ^ greater than or equal
-               | LTh  -- ^ less than
-               | GTh  -- ^ greater than
-               | LE   -- ^ less than or equal
-               | AL   -- ^ always
-               deriving (Eq)
+data Condition = 
+      Eq   -- ^ Equal
+    | Neq  -- ^ Not equal
+    | CS   -- ^ Carry set
+    | CC   -- ^ Carry clear
+    | MI   -- ^ negative
+    | PL   -- ^ zero or positive
+    | VS   -- ^ overflow
+    | VC   -- ^ no overflow
+    | HI   -- ^ unsigned higher
+    | LS   -- ^ unsigned lower or same
+    | GE   -- ^ greater than or equal
+    | LTh  -- ^ less than
+    | GTh  -- ^ greater than
+    | LE   -- ^ less than or equal
+    | AL   -- ^ always
+  deriving (Eq)
 
 instance PrintARM Instr where
   printARM (Section str)                  = '.' : str  
@@ -152,54 +159,54 @@ instance PrintARM Instr where
  
 
 instance PrintARM Op2 where
- printARM (ShiftReg reg shift)           = printARM (reg) ++ printARM (shift)
- printARM (ImmOpInt int)                 = "#" ++ show int
- printARM (ImmOpCh '\0')                 = "#'\\0'"
- printARM (ImmOpCh char)                 = "#" ++ show char
+  printARM (ShiftReg reg shift) = printARM (reg) ++ printARM (shift)
+  printARM (ImmOpInt int)       = "#" ++ show int
+  printARM (ImmOpCh '\0')       = "#'\\0'"
+  printARM (ImmOpCh char)       = "#" ++ show char
 
 instance PrintARM Shift where
- printARM (ASL i) = ", ASL #" ++ show i
- printARM (LSL i) = ", LSL #" ++ show i
- printARM (LSR i) = ", LSR #" ++ show i
- printARM (ASR i) = ", ASR #" ++ show i
- printARM (ROR i) = ", ROR #" ++ show i
- printARM (RRX)   = ", RRX"
- printARM (NSH)   = ""
+  printARM (ASL i) = ", ASL #" ++ show i
+  printARM (LSL i) = ", LSL #" ++ show i
+  printARM (LSR i) = ", LSR #" ++ show i
+  printARM (ASR i) = ", ASR #" ++ show i
+  printARM (ROR i) = ", ROR #" ++ show i
+  printARM (RRX)   = ", RRX"
+  printARM (NSH)   = ""
 
 instance PrintARM Mem where
- printARM (UB) = "B"
- printARM (SB) = "SB"
- printARM (H)  = "H"
- printARM (SH) = "SH"
- printARM (W)  = ""
+  printARM (UB) = "B"
+  printARM (SB) = "SB"
+  printARM (H)  = "H"
+  printARM (SH) = "SH"
+  printARM (W)  = ""
 
 instance PrintARM Address where
- printARM (Label str)               = "=" ++ str
- printARM (Const i)                 = "=" ++ show i
- printARM (OffReg reg offset True)  = "[" ++ printARM (reg) ++ printARM (offset) ++ "]" ++ "!"
- printARM (OffReg reg offset False) = "[" ++ printARM (reg) ++ printARM (offset) ++ "]"
+  printARM (Label str)               = "=" ++ str
+  printARM (Const i)                 = "=" ++ show i
+  printARM (OffReg reg offset True)  = "[" ++ printARM (reg) ++ printARM (offset) ++ "]" ++ "!"
+  printARM (OffReg reg offset False) = "[" ++ printARM (reg) ++ printARM (offset) ++ "]"
 
 instance PrintARM Offset where
- printARM (Int 0)              = ""
- printARM (Int i)              = ", #" ++ show i 
- printARM (RegShift reg shift) = ", " ++ printARM(reg) ++ printARM (shift)
+  printARM (Int 0)              = ""
+  printARM (Int i)              = ", #" ++ show i 
+  printARM (RegShift reg shift) = ", " ++ printARM(reg) ++ printARM (shift)
 
 instance PrintARM Condition where
-   printARM (Eq)  = "EQ"
-   printARM (Neq) = "NE"
-   printARM (CS)  = "CS"
-   printARM (CC)  = "CC"
-   printARM (MI)  = "MI"
-   printARM (PL)  = "PL"
-   printARM (VS)  = "VS"
-   printARM (VC)  = "VC"
-   printARM (HI)  = "HI"
-   printARM (LS)  = "LS"
-   printARM (GE)  = "GE"
-   printARM (LTh) = "LT"
-   printARM (GTh) = "GT"
-   printARM (LE)  = "LE"
-   printARM (AL)  = ""
+  printARM (Eq)  = "EQ"
+  printARM (Neq) = "NE"
+  printARM (CS)  = "CS"
+  printARM (CC)  = "CC"
+  printARM (MI)  = "MI"
+  printARM (PL)  = "PL"
+  printARM (VS)  = "VS"
+  printARM (VC)  = "VC"
+  printARM (HI)  = "HI"
+  printARM (LS)  = "LS"
+  printARM (GE)  = "GE"
+  printARM (LTh) = "LT"
+  printARM (GTh) = "GT"
+  printARM (LE)  = "LE"
+  printARM (AL)  = ""
 
 instance PrintARM Reg where
   printARM(R0)           = "r0"
@@ -224,9 +231,9 @@ instance Bounded Reg where
   maxBound = R12
 
 instance PrintARM [Reg] where
- printARM ([])   = []
- printARM (r:rs) = '{' : concat ((printARM r) : [", " ++ printARM reg | reg <- rs]) ++ "}"
+  printARM ([])   = []
+  printARM (r:rs) = '{' : concat ((printARM r) : [", " ++ printARM reg | reg <- rs]) ++ "}"
 
 instance PrintARM Set where
- printARM(T) = "S"
- printARM(F) = ""
+  printARM(T) = "S"
+  printARM(F) = ""
